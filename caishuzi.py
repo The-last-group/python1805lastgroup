f263c70375fa2564ec664fc238d761d74777293e
xuanxxx <2827223348@qq.com>
2018-10-15 21:03:23 +0800

    python

--- caishuzi.py
+++ caishuzi.py
@@ -5,13 +5,13 @@ z = int(random.randint(1,1000))
 print(z)
 print('游戏名称:猜数字')
 a = int(input('请输入一个1-1000之间的数:'))
-while (a!=z):
-    if a>z:
+while (a != z):
+    if a > z:
         print('你输入的是',a,end='')
-        a=int(input('太大了,重新输入:'))
-        x=x+1
-    if a<z:
-        print('你输入的是', a)
+        a = int(input('太大了,重新输入:'))
+        x = x+1
+    if a < z:
+        print('你输入的是', a,end='')
         a = int(input('太小了，重新输入：'))
         y = y + 1
     s = x + y + 1
@@ -25,7 +25,7 @@ else:
         print('你是爱因斯坦在世！一共只用了{}步！'.format(s))
     elif 5 >= s > 3:
         print('你是个聪明的孩子!一共只用了{}步！'.format(s))
-    elif 10>= s>5:
+    elif 10 >= s > 5:
         print('你是个高手!一共只用了{}步!'.format(s))
     else:
         print('辛苦你了！你尝试了{}次，终于正确了！'.format(s))
